061459f13d4c247bffc00c5445e665311d23b30d
LtdSauce <46743879+LtdSauce@users.noreply.github.com>
2023-01-27 23:35:45 +0100

    fix regular expression for pack url

--- update_vendor_files/src/packs.rs
+++ update_vendor_files/src/packs.rs
@@ -3,16 +3,15 @@ use regex::Regex;
 #[derive(Debug)]
 pub struct DownloadablePacks {
     html_page: String,
-    packs_url: String,
 }
 
 impl DownloadablePacks {
+    const PACKS_URL: &str = "https://packs.download.microchip.com/";
+
     pub fn from_microchip_website() -> Result<DownloadablePacks, Box<dyn std::error::Error>> {
-        let packs_download_microchip_url = "https://packs.download.microchip.com/";
         // TODO improve error handling
         Ok(DownloadablePacks {
-            html_page: reqwest::blocking::get(packs_download_microchip_url)?.text()?,
-            packs_url: packs_download_microchip_url.to_string(),
+            html_page: reqwest::blocking::get(DownloadablePacks::PACKS_URL)?.text()?
         })
     }
 
@@ -31,13 +30,13 @@ impl DownloadablePacks {
             name: name.to_string(),
             path_template: format!(
                 "{}/Microchip.{pack_name}.{{version}}.atpack",
-                self.packs_url
+                DownloadablePacks::PACKS_URL
             ),
             selected_version: None,
             available_versions: Vec::new(),
         };
         let re = Regex::new(&format!(
-            r#"Microchip\.{pack_name}\.(\d.*\d.*\d.*)\.atpack"#
+            r#"Microchip\.{pack_name}\.(\d*\.\d*\.\d*)\.atpack"#
         ))?;
         for captures in re.captures_iter(&self.html_page) {
             pack.available_versions.push(String::from(&captures[1]))
@@ -47,6 +46,7 @@ impl DownloadablePacks {
 }
 
 #[derive(Debug)]
+#[derive(PartialEq)]
 pub struct DownloadablePack {
     name: String,
     path_template: String,
@@ -56,5 +56,19 @@ pub struct DownloadablePack {
 
 #[cfg(test)]
 mod tests {
-    // TODO write tests!
+    use crate::packs::{DownloadablePack, DownloadablePacks};
+
+    #[test]
+    #[should_panic]
+    fn controller_family_panics_if_controller_not_supported() {
+        DownloadablePacks { html_page: "BobAndMarry".to_string() }
+            .controller_family("hurgel").unwrap();
+    }
+
+    #[test]
+    fn controller_family_finds_every_version_for_family() {
+        let packs = DownloadablePacks { html_page: "BobAndMarry,\nMicrochip.ATmega_DFP.1.atpack//Microchip.ATmega_DFP.1.2.4.atpack".to_string() }
+            .controller_family("atmega").unwrap();
+        assert_eq!(packs.available_versions, vec!("1.2.4"))
+    }
 }
