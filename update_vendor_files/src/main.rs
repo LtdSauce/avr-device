6d440d27d63b0adb00df362eca858631c7958e3d
LtdSauce <46743879+LtdSauce@users.noreply.github.com>
2023-01-28 23:22:52 +0100

    select newest version if none is selected and refactor usage

--- update_vendor_files/src/main.rs
+++ update_vendor_files/src/main.rs
@@ -1,7 +1,17 @@
 mod packs;
 
+use std::env;
+
+fn parse_args(args: Vec<String>) -> packs::PackInfo {
+    // ToDo emit error message and usage string
+    packs::PackInfo::from_str(&args[1])
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let pack = packs::DownloadablePacks::from_microchip_website()?.controller_family("atmega")?;
+    let pack = parse_args(env::args().collect());
+    let pack = packs::DownloadablePacks::from_microchip_website()?.for_pack(&pack)?;
     println!("{pack:?}");
     Ok(())
 }
+
+
--- update_vendor_files/src/packs.rs
+++ update_vendor_files/src/packs.rs
@@ -2,6 +2,38 @@ use regex::Regex;
 
 // TODO improve error handling: at least swap panics with result errors?
 
+#[derive(PartialEq)]
+#[derive(Debug)]
+pub struct PackInfo {
+    pub name: String,
+    pub selected_version: Option<String>,
+}
+
+impl PackInfo {
+    pub fn from_str(arg: &str) -> PackInfo {
+        let mut splitted = arg.split("==");
+        PackInfo {
+            name: splitted.next().unwrap().to_string(),
+            selected_version: splitted.next().and_then(|s| Some(s.to_string())),
+        }
+    }
+
+    fn from(name: &str, version: &str) -> PackInfo {
+        PackInfo {
+            name: name.to_string(),
+            selected_version: Some(version.to_string()),
+        }
+    }
+
+    fn from_name(name: &str) -> PackInfo {
+        PackInfo {
+            name: name.to_string(),
+            selected_version: None,
+        }
+    }
+}
+
+
 #[derive(Debug)]
 pub struct DownloadablePacks {
     html_page: String,
@@ -16,35 +48,39 @@ impl DownloadablePacks {
         })
     }
 
-    pub fn controller_family(
+    pub fn for_pack(
         &self,
-        name: &str,
+        pack: &PackInfo,
     ) -> Result<DownloadablePack, Box<dyn std::error::Error>> {
         // The whole project uses the lowercase representation, so we do that too and convert it here
         // Additionally, those packs have some suffixes which are applied here
-        let pack_name = match name {
+        let pack_name = match pack.name.as_str() {
             "atmega" => "ATmega_DFP",
-            _ => panic!("{name} is not a valid controller family!"),
+            _ => panic!("{} is not a valid controller family!", pack.name),
         };
 
-        let mut pack = DownloadablePack {
-            name: name.to_string(),
-            path_template: format!(
+        Ok(DownloadablePack::new(
+            pack.name.clone(),
+            format!(
                 "{}Microchip.{pack_name}.{}.atpack",
                 DownloadablePacks::PACKS_URL,
                 DownloadablePack::VERSION_PLACEHOLDER
             ),
-            selected_version: None,
-            available_versions: Vec::new(),
-        };
-        let re = Regex::new(&format!(
-            r#"Microchip\.{pack_name}\.(\d*\.\d*\.\d*)\.atpack"#
-        ))?;
-        for captures in re.captures_iter(&self.html_page) {
-            pack.available_versions.push(String::from(&captures[1]))
-        }
-        Ok(pack)
+            pack.selected_version.clone(),
+            parse_available_versions_from_html(self.html_page.as_str(), pack_name)?,
+        ))
     }
+}
+
+fn parse_available_versions_from_html(html: &str, pack_name: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let mut available_versions: Vec<String> = Vec::new();
+    let re = Regex::new(&format!(
+        r#"Microchip\.{}\.(\d*\.\d*\.\d*)\.atpack"#, pack_name.to_string()
+    ))?;
+    for captures in re.captures_iter(html) {
+        available_versions.push(String::from(&captures[1]))
+    }
+    Ok(available_versions)
 }
 
 #[derive(Debug)]
@@ -60,19 +96,27 @@ impl DownloadablePack {
     const VERSION_PLACEHOLDER: &str = "{version}";
 
 
-    pub fn version(&mut self, version: &str) -> &DownloadablePack {
-        if !self.available_versions.contains(&version.to_string()) {
-            panic!("pack '{}' does not support selected version {version}. Possible versions are: {:?}",
-                   self.name, self.available_versions)
+    pub fn new(name: String,
+               path_template: String,
+               selected_version: Option<String>,
+               available_versions: Vec<String>, ) -> DownloadablePack {
+        println!("{available_versions:?}");
+        if selected_version.is_some() && !available_versions.contains(selected_version.as_ref().unwrap()) {
+            panic!("pack '{}' does not support selected version {}. Possible versions are: {:?}",
+                   name, selected_version.as_ref().unwrap(), available_versions)
         }
-        self.selected_version = Option::from(version.to_string());
-        self
+        DownloadablePack {
+            name: name.clone(),
+            path_template: path_template.clone(),
+            available_versions: available_versions.clone(),
+            selected_version: selected_version.clone(),
+        }
     }
 
     fn url(&self) -> Result<String, Box<dyn std::error::Error>> {
         let url = self.path_template.replace(
             DownloadablePack::VERSION_PLACEHOLDER,
-            self.selected_version.as_ref().unwrap(),
+            self.selected_version.as_ref().or(self.available_versions.iter().max()).unwrap(),
         );
         Ok(url)
     }
@@ -83,46 +127,86 @@ impl DownloadablePack {
 }
 
 #[cfg(test)]
-mod downloadable_packs_tests {
-    use crate::packs::DownloadablePacks;
+mod tests {
+    use crate::packs::parse_available_versions_from_html;
 
     #[test]
-    #[should_panic]
-    fn controller_family_panics_if_controller_not_supported() {
-        DownloadablePacks { html_page: "BobAndMarry".to_string() }
-            .controller_family("hurgel").unwrap();
+    fn parse_available_versions_from_html_returns_every_contained_version_for_pack() {
+        let versions = parse_available_versions_from_html(
+            "BobAndMarry,\nMicrochip.ATmega_DFP.1.atpack//Microchip.ATmega_DFP.1.2.4.atpack",
+            "ATmega_DFP").unwrap();
+        assert_eq!(versions, vec!("1.2.4"))
+    }
+}
+
+#[cfg(test)]
+mod pack_tests {
+    use crate::packs::PackInfo;
+
+    #[test]
+    fn parse_controller_detects_name_and_version() {
+        assert_eq!(PackInfo::from_str("Foo==1.1"), PackInfo { name: "Foo".to_string(), selected_version: Some("1.1".to_string()) });
     }
 
     #[test]
-    fn controller_family_finds_every_version_for_family() {
+    fn from_str_returns_none_version_if_omitted() {
+        assert_eq!(PackInfo::from_str("Foo"), PackInfo { name: "Foo".to_string(), selected_version: None });
+    }
+}
+
+#[cfg(test)]
+mod downloadable_packs_tests {
+    use crate::packs::{DownloadablePacks, PackInfo};
+
+    #[test]
+    #[should_panic]
+    fn for_pack_panics_if_controller_not_supported() {
+        DownloadablePacks { html_page: "BobAndMarry".to_string() }
+            .for_pack(&PackInfo::from_name("hurgel")).unwrap();
+    }
+
+    #[test]
+    fn for_pack_finds_every_version_for_family() {
         let packs = DownloadablePacks { html_page: "BobAndMarry,\nMicrochip.ATmega_DFP.1.atpack//Microchip.ATmega_DFP.1.2.4.atpack".to_string() }
-            .controller_family("atmega").unwrap();
+            .for_pack(&PackInfo::from_name("atmega")).unwrap();
         assert_eq!(packs.available_versions, vec!("1.2.4"))
     }
 }
 
 #[cfg(test)]
 mod downloadable_pack_test {
-    use crate::packs::{DownloadablePack, DownloadablePacks};
+    use crate::packs::{DownloadablePack, DownloadablePacks, PackInfo};
 
     #[test]
     #[should_panic]
-    fn version_panics_on_unsupported_version_selected() {
-        let mut pack = DownloadablePack {
-            name: "Bob".to_string(),
-            path_template: "".to_string(),
-            selected_version: None,
-            available_versions: vec!["".to_string()],
-        };
-        pack.version("1.1.1");
+    fn new_panics_on_unsupported_version_selected() {
+        DownloadablePack::new(
+            "Bob".to_string(),
+            "".to_string(),
+            Some("1.1.1".to_string()),
+            vec!["".to_string()],
+        );
+    }
+
+    fn check_url(pack: &DownloadablePack, expected_url: &str) {
+        assert_eq!(pack.url().unwrap(), expected_url)
     }
 
     #[test]
     fn url_returns_download_url_for_selected_version_if_present() {
-        let url = DownloadablePacks {
+        let pack = DownloadablePacks {
             html_page: "BobAndMarry,\nMicrochip.ATmega_DFP.1.1.2.atpack\
         //Microchip.ATmega_DFP.1.2.4.atpack".to_string()
-        }.controller_family("atmega").unwrap().version("1.1.2").url();
-        assert_eq!(url.unwrap(), "https://packs.download.microchip.com/Microchip.ATmega_DFP.1.1.2.atpack")
+        }.for_pack(&PackInfo::from("atmega", "1.1.2")).unwrap();
+        check_url(&pack, "https://packs.download.microchip.com/Microchip.ATmega_DFP.1.1.2.atpack")
+    }
+
+    #[test]
+    fn url_returns_newest_version_if_none_selected() {
+        let pack = DownloadablePacks {
+            html_page: "BobAndMarry,\nMicrochip.ATmega_DFP.1.1.2.atpack\
+        //Microchip.ATmega_DFP.1.2.4.atpack".to_string()
+        }.for_pack(&PackInfo::from_str("atmega")).unwrap();
+        check_url(&pack, "https://packs.download.microchip.com/Microchip.ATmega_DFP.1.2.4.atpack")
     }
 }
