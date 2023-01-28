182edf0979d3a99c6b337ca50885e07403776aad
LtdSauce <46743879+LtdSauce@users.noreply.github.com>
2023-01-28 21:40:55 +0100

    panic on unsupported version early

--- update_vendor_files/src/packs.rs
+++ update_vendor_files/src/packs.rs
@@ -1,5 +1,7 @@
 use regex::Regex;
 
+// TODO improve error handling: at least swap panics with result errors?
+
 #[derive(Debug)]
 pub struct DownloadablePacks {
     html_page: String,
@@ -9,7 +11,6 @@ impl DownloadablePacks {
     const PACKS_URL: &str = "https://packs.download.microchip.com/";
 
     pub fn from_microchip_website() -> Result<DownloadablePacks, Box<dyn std::error::Error>> {
-        // TODO improve error handling
         Ok(DownloadablePacks {
             html_page: reqwest::blocking::get(DownloadablePacks::PACKS_URL)?.text()?
         })
@@ -29,8 +30,9 @@ impl DownloadablePacks {
         let mut pack = DownloadablePack {
             name: name.to_string(),
             path_template: format!(
-                "{}Microchip.{pack_name}.{{version}}.atpack",
-                DownloadablePacks::PACKS_URL
+                "{}Microchip.{pack_name}.{}.atpack",
+                DownloadablePacks::PACKS_URL,
+                DownloadablePack::VERSION_PLACEHOLDER
             ),
             selected_version: None,
             available_versions: Vec::new(),
@@ -55,15 +57,23 @@ pub struct DownloadablePack {
 }
 
 impl DownloadablePack {
+    const VERSION_PLACEHOLDER: &str = "{version}";
+
+
     pub fn version(&mut self, version: &str) -> &DownloadablePack {
-        if self.available_versions.contains(&version.to_string()) {
-            self.selected_version = Option::from(version.to_string());
+        if !self.available_versions.contains(&version.to_string()) {
+            panic!("pack '{}' does not support selected version {version}. Possible versions are: {:?}",
+                   self.name, self.available_versions)
         }
+        self.selected_version = Option::from(version.to_string());
         self
     }
 
     fn url(&self) -> Result<String, Box<dyn std::error::Error>> {
-        let url = self.path_template.replace("{version}", self.selected_version.as_ref().unwrap());
+        let url = self.path_template.replace(
+            DownloadablePack::VERSION_PLACEHOLDER,
+            self.selected_version.as_ref().unwrap(),
+        );
         Ok(url)
     }
 
@@ -73,7 +83,7 @@ impl DownloadablePack {
 }
 
 #[cfg(test)]
-mod tests {
+mod downloadable_packs_tests {
     use crate::packs::DownloadablePacks;
 
     #[test]
@@ -89,9 +99,26 @@ mod tests {
             .controller_family("atmega").unwrap();
         assert_eq!(packs.available_versions, vec!("1.2.4"))
     }
+}
+
+#[cfg(test)]
+mod downloadable_pack_test {
+    use crate::packs::{DownloadablePack, DownloadablePacks};
 
     #[test]
-    fn downloadable_pack_returns_download_url_for_selected_version() {
+    #[should_panic]
+    fn version_panics_on_unsupported_version_selected() {
+        let mut pack = DownloadablePack {
+            name: "Bob".to_string(),
+            path_template: "".to_string(),
+            selected_version: None,
+            available_versions: vec!["".to_string()],
+        };
+        pack.version("1.1.1");
+    }
+
+    #[test]
+    fn url_returns_download_url_for_selected_version_if_present() {
         let url = DownloadablePacks {
             html_page: "BobAndMarry,\nMicrochip.ATmega_DFP.1.1.2.atpack\
         //Microchip.ATmega_DFP.1.2.4.atpack".to_string()
